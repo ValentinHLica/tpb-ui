02a5e6c878c5efca5db630119382a9fca2bf90be
bashvtini <bashvtini@gmail.com>
2020-05-14 21:22:26 +0200

    Updates

---
+++ precache-manifest.4079402f00ecee5810f45af6d19c09d8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d4a09d5f9f448c746051afb992f75b",
+    "url": "/tpb-ui/index.html"
+  },
+  {
+    "revision": "a80f22040d9f1991d586",
+    "url": "/tpb-ui/static/css/main.93e6ad9b.chunk.css"
+  },
+  {
+    "revision": "d97c4d83bee2937646e6",
+    "url": "/tpb-ui/static/js/2.abe1ec20.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tpb-ui/static/js/2.abe1ec20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a80f22040d9f1991d586",
+    "url": "/tpb-ui/static/js/main.2854b464.chunk.js"
+  },
+  {
+    "revision": "2174ca9f7ad4553f4a26",
+    "url": "/tpb-ui/static/js/runtime-main.13f49e95.js"
+  },
+  {
+    "revision": "abc59452d78316ace408d807fd41af67",
+    "url": "/tpb-ui/static/media/check.abc59452.svg"
+  },
+  {
+    "revision": "ea3f87d138d7aecf39f43afa7707da47",
+    "url": "/tpb-ui/static/media/clock.ea3f87d1.svg"
+  },
+  {
+    "revision": "a89f493303987f577210ffb8623c6802",
+    "url": "/tpb-ui/static/media/download.a89f4933.svg"
+  },
+  {
+    "revision": "d4857d8daf2d8c2823bb48cae80908e7",
+    "url": "/tpb-ui/static/media/loader.d4857d8d.svg"
+  },
+  {
+    "revision": "590f7a385f1727b8519c6a6f305d891c",
+    "url": "/tpb-ui/static/media/menu.590f7a38.svg"
+  },
+  {
+    "revision": "a450ec36f430e2eab04ace41e135f72f",
+    "url": "/tpb-ui/static/media/search.a450ec36.svg"
+  },
+  {
+    "revision": "48b719bc72f8b5660204f0d21e338c66",
+    "url": "/tpb-ui/static/media/star.48b719bc.svg"
+  },
+  {
+    "revision": "14dd1331c8efa5f93a329e56662a60f1",
+    "url": "/tpb-ui/static/media/upload.14dd1331.svg"
+  }
+]);
