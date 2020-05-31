9905788acee7b269cec8feb64b20ede5051217fd
bashvtini <bashvtini@gmail.com>
2020-05-31 19:12:09 +0200

    final

--- src/components/Results/Header.js
+++ src/components/Results/Header.js
@@ -4,12 +4,10 @@ import Logo from "../other/logo";
 
 import { Link } from "react-router-dom";
 
-import DropDownIcone from "../../assets/img/menu.svg";
 import SearchIcone from "../../assets/img/search.svg";
 
 export default function Header(props) {
   const [emptyQuery, setemptyQuery] = useState(false);
-  const [dropdown, setDropdown] = useState(false);
 
   const submit = async (query, form) => {
     form.preventDefault();
@@ -24,7 +22,6 @@ export default function Header(props) {
     }
 
     props.fetchData(query, props.category, props.sort);
-    setDropdown(false);
   };
 
   return (
@@ -51,37 +48,9 @@ export default function Header(props) {
             </button>
           </form>
 
-          <div className="header-wrapper">
-            <ul className="list">
-              <li>Login</li>
-              <li>Register</li>
-            </ul>
-
-            <div
-              className="dropdown-button"
-              onClick={() => {
-                setDropdown(!dropdown);
-              }}
-            >
-              <img src={DropDownIcone} alt="DropDown Icone" />
-            </div>
-          </div>
+          <div className="header-wrapper"></div>
         </div>
       </div>
-
-      {dropdown ? (
-        <div
-          className="bc"
-          onClick={() => {
-            setDropdown(!dropdown);
-          }}
-        ></div>
-      ) : null}
-
-      <ul className={`dropdown-menu ${dropdown ? "dropdown-menu-open" : ""}`}>
-        <li>Login</li>
-        <li>Register</li>
-      </ul>
     </React.Fragment>
   );
 }
--- src/components/Torrent/Header.js
+++ src/components/Torrent/Header.js
@@ -4,13 +4,11 @@ import { Link } from "react-router-dom";
 
 import Logo from "../other/logo";
 
-import DropDownIcone from "../../assets/img/menu.svg";
 import SearchIcone from "../../assets/img/search.svg";
 
 export default function Header(props) {
   const [query, setQuery] = useState("");
   const [emptyQuery, setEmptyQuery] = useState(false);
-  const [dropdown, setDropdown] = useState(false);
 
   const submit = (quickSearch, form) => {
     form.preventDefault();
@@ -25,7 +23,6 @@ export default function Header(props) {
     }
 
     quickSearch(query);
-    setDropdown(false);
   };
 
   return (
@@ -52,37 +49,9 @@ export default function Header(props) {
             </button>
           </form>
 
-          <div className="header-wrapper">
-            <ul className="list">
-              <li>Login</li>
-              <li>Register</li>
-            </ul>
-
-            <div
-              className="dropdown-button"
-              onClick={() => {
-                setDropdown(!dropdown);
-              }}
-            >
-              <img src={DropDownIcone} alt="DropDown Icone" />
-            </div>
-          </div>
+          <div></div>
         </div>
       </div>
-
-      {dropdown ? (
-        <div
-          className="bc"
-          onClick={() => {
-            setDropdown(!dropdown);
-          }}
-        ></div>
-      ) : null}
-
-      <ul className={`dropdown-menu ${dropdown ? "dropdown-menu-open" : ""}`}>
-        <li>Login</li>
-        <li>Register</li>
-      </ul>
     </React.Fragment>
   );
 }
